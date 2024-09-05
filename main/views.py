60010846279b4bcffe413ac888364a0e5d4cc869
Veynnz <diodavis123@gmail.com>
2024-09-05 22:03:09 +0700

    First commit

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'app' : 'JualJual',
+        'npm' : '2306213836',
+        'name': 'Dionysius Davis',
+        'class': 'PBP C'
+    }
+
+    return render(request, "main.html", context)
